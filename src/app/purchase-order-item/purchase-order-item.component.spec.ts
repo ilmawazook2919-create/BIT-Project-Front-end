e041fe5b01259ef50662198533fb04304a45be19
ilmawazook2919-create <ilmawazook2919@gmail.com>
2026-06-06 09:55:28 +0530

    updated to the purchase order item

---
+++ src/app/purchase-order-item/purchase-order-item.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PurchaseOrderItemComponent } from './purchase-order-item.component';
+
+describe('PurchaseOrderItemComponent', () => {
+  let component: PurchaseOrderItemComponent;
+  let fixture: ComponentFixture<PurchaseOrderItemComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PurchaseOrderItemComponent]
+    });
+    fixture = TestBed.createComponent(PurchaseOrderItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
